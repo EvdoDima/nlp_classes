3854b499e1a5bcf6512786218fd6673805319a43
Evdodima <evdodima@gmail.com>
2017-04-10 11:45:36 +0300

    merge

--- train.py
+++ train.py
@@ -1,10 +1,6 @@
 from nltk.model import NgramModel
 import nltk
-<<<<<<< HEAD
-import os, pickle
-=======
 import os, dill, pickle
->>>>>>> f4d6cfffffe2fcd6ee1cb7406189106ca01d75f3
 
 text = ""
 with open (os.getcwd() + "/input/1.txt", "r") as file:
@@ -13,8 +9,5 @@ with open (os.getcwd() + "/input/1.txt", "r") as file:
 # print(text)
 
 lm = NgramModel(3, nltk.word_tokenize(text))
-<<<<<<< HEAD
-pickle.dump(lm, open("out.txt", "wb"),protocol=pickle.HIGHEST_PROTOCOL)
-=======
+
 pickle.dump(lm, open("out.txt", "wb"))
->>>>>>> f4d6cfffffe2fcd6ee1cb7406189106ca01d75f3
