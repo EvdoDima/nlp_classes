a0dfb9dcfb264987505f6f408b9f51c4ce1cc1a2
Rinat Khanov <rinat@khanov.com>
2017-04-10 11:39:53 +0300

    shit

---
+++ train.py
@@ -0,0 +1,12 @@
+from nltk.model import NgramModel
+import nltk
+import os, pickle
+
+text = ""
+with open (os.getcwd() + "/input/1.txt", "r") as file:
+	text = file.read()
+
+# print(text)
+
+lm = NgramModel(3, nltk.word_tokenize(text))
+pickle.dump(lm, open("out.txt", "wb"),protocol=pickle.HIGHEST_PROTOCOL)
